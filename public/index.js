766f33b927a47038492697ecb668cdc055e167d8
Dheeraj Patidar <115142164+mrdheerajpatidar@users.noreply.github.com>
2024-11-11 13:48:07 +0000

    responsiv nav

--- public/index.js
+++ public/index.js
@@ -20,3 +20,18 @@ video.addEventListener("click", () => {
     video.removeAttribute("controls");
   }
 });
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+const navLinks = document.querySelectorAll(".nav-link");
+navLinks.forEach((link) =>
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
